a76410a0dcfd606bb97b319e85806ff67048aa74
Jean-marc WANCTIN <jewancti@e2r7p6.clusters.42paris.fr>
2023-03-02 07:26:39 +0100

    remove endl for each words in megepahone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jewancti <jewancti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 23:59:03 by jewancti          #+#    #+#             */
-/*   Updated: 2023/03/02 06:47:16 by jewancti         ###   ########.fr       */
+/*   Updated: 2023/03/02 07:19:01 by jewancti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ int main(int ac, char **av)
 			char *const tmp = av[i];
 			for (int j = 0; tmp[j]; j++)
 				tmp[j] = std :: toupper(tmp[j]);
-			std :: cout << tmp << std :: endl;
+			std :: cout << tmp;
 		}
+		std :: cout << std :: endl;
 	}
 	return (0);
 }
